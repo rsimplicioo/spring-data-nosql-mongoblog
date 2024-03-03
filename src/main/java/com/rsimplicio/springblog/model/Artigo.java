f8264032c18b07565ef44de2fa97d68d08ae3fab
rsimplicioo <simplicio.rsb@gmail.com>
2024-03-03 00:07:35 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/rsimplicio/springblog/model/Artigo.java
@@ -0,0 +1,18 @@
+package com.rsimplicio.springblog.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+@Document
+@Data
+public class Artigo {
+    @Id
+    private String codigo;
+    private String titulo;
+    private LocalDateTime data;
+    private String texto;
+    private String url;
+    private Integer status;
+}
